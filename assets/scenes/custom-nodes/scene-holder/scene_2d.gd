78f04c8a95ab91f2a40e5c4bfb91badd91ccd9c2
Morne <morneventer.mv@gmail.com>
2024-02-08 12:46:13 +0200

    add vim support

--- assets/scenes/custom-nodes/scene-holder/scene_2d.gd
+++ assets/scenes/custom-nodes/scene-holder/scene_2d.gd
@@ -1,2 +1,11 @@
+@tool
 extends SubViewport
 class_name Scene2D
+
+const BAD_PARENT_ERROR := "This nodes needs to be a child of a SceneHolder node."
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var parent := get_parent()
+	if not parent is SceneHolder:
+		return [BAD_PARENT_ERROR]
+	return []
