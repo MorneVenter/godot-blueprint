120f7e6af8857f1a12182139c23e170c50c3a3f5
Morne Venter <morne@Mornes-MacBook-Pro.local>
2024-06-08 16:41:34 +0200

    refactor save system

--- assets/scripts/managers/save_manager.gd
+++ assets/scripts/managers/save_manager.gd
@@ -51,7 +51,7 @@ func _update_save_file() -> void:
 	save_game.store_var(_data)
 
 
-func _delete_save(id: int) -> void:
+func _delete_save_data(id: int) -> void:
 	var save_path: String = _get_save_path(id)
 	var directory := DirAccess.open(_USERFOLDER)
 	directory.remove(save_path)
@@ -62,16 +62,20 @@ func _unload_and_reset() -> void:
 	_save_id = 1
 
 
-func delete(slot: int) -> void:
-	_delete_save(slot)
-	_unload_and_reset()
+## Deletes a save slot based on its slot number. If the deleted save is currently in the active save slot, it will clear the currently loaded data. Call load_save to load a save file again.
+func delete_save(slot: int) -> void:
+	_delete_save_data(slot)
+	if slot == _save_id:
+		_unload_and_reset()
 
 
+## Loads a save file into the active save slot. Takes in a save slot number. If the save file does not exist it will be created.
 func load_save(slot: int) -> void:
 	_save_id = slot
 	_load_save_data()
 
 
+## Saves a value to the currently loaded save file based on a unique string key,
 func save_value(my_key: String, value: Variant) -> void:
 	if value == null:
 		return
@@ -80,16 +84,19 @@ func save_value(my_key: String, value: Variant) -> void:
 	_update_save_file()
 
 
-func load_value(my_key: String) -> Variant:
+## Gets a value from the currently loaded save file based on a unique string key.
+## Optionally, provide a default return value as the second parameter to assist with typed returns. Will return null by default.
+func load_value(my_key: String, default_value: Variant = null) -> Variant:
 	if _data.has(my_key):
 		var loaded_value: Variant = _data[my_key]
 		if loaded_value == null:
-			return null
+			return default_value
 		return loaded_value
 	else:
-		return null
+		return default_value
 
 
+## Gets the last date and time the current loaded save file was written to in the format (YYYY-MM-DDTHH:MM:SS).
 func get_last_write_date() -> String:
-	var date_and_time: String = load_value(_DATE_KEY)
+	var date_and_time: String = load_value(_DATE_KEY, "")
 	return date_and_time
--- main.gd
+++ main.gd
@@ -1,5 +1,1 @@
 extends Node
-
-
-func _ready() -> void:
-	pass
